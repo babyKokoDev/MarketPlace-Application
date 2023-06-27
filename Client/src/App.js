13f15e24fc4bf71d7eb092bdbc3f6e6ed4eb4f1d
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-06-27 09:16:52 +0100

    project initialization

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,12 @@
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
+      <h1>
          MARKET PLACE
+      </h1>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
--- Client/src/index.js
+++ Client/src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ConfigProvider
+     theme = {{
+      components : {
+        Button : {
+          colorPrimary : '#40513B',
+          colorPrimaryHover : '#40513B',
+          borderRadius : '0'
+        }
+      }
+     }}
+    >
     <App />
+    </ConfigProvider>
+   
   </React.StrictMode>
 );
 
