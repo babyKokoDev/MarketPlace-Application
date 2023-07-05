89832713895e202e9a30b40b67df86bf8cc7c852
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-07-05 23:48:42 +0100

    router

--- Client/src/pages/Home/index.js
+++ Client/src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='bg-red-500 text-2xl font-bold text-center '>Home</div>
+    <div className='bg-red-500 text-2xl font-bold text-center '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dignissimos distinctio, totam commodi sit similique numquam voluptates, labore amet ipsam repellendus! Quo ipsa quam maxime ea quasi fuga obcaecati, neque eveniet deleniti ipsum. Dolor voluptate laudantium soluta numquam debitis explicabo nesciunt voluptas maiores iure officiis? Iusto magni aliquid porro laudantium?</div>
   )
 }
 
