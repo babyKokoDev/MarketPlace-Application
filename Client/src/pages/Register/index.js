25f50ece082c15d229c41e452645538e821cb815
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-07-06 12:01:50 +0100

    registration page

--- Client/src/pages/Register/index.js
+++ Client/src/pages/Register/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import {Button, Form, Input} from 'antd'
 import { Link } from 'react-router-dom'
+import Divider from '../../components/Divider'
 
 const Register = () => {
   return (
     <div className='h-screen bg-primary flex justify-center items-center'>
          <div className='bg-white p-5 rounded w-[450px]'>
+           <h1 className='text-primary text-2xl'>REGISTER HERE</h1>
+             <Divider />
              <Form layout='vertical'>
                <Form.Item label="Name" name='name'>
                  <Input placeholder='Name' />
