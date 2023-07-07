33d6b3ddde229904537ecbefcee872a3dd519166
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-07-07 09:29:46 +0100

    connecting to the backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+const connectDb = require('./dB/connect')
 const port = process.env.PORT || 5000
 
 
+const start = async () => {
+    try {
+         await connectDb(process.env.MONGO_URI)
+         app.listen(port, ()=>{
+            console.log(`Server is working and listening on port ${port}`);
+            console.log(process.env.MONGO_URI)
+        })
+    } catch (error) {
+          console.log(error)   
+    }
+}
 
-app.listen(port, ()=>{
-    console.log(`Server is working and listening on port ${port}`);
-})
+start()
