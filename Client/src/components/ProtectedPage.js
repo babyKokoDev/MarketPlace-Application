cb5daae1a72e047f27185c5d99d58f1b6859cc5b
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-07-12 23:55:28 +0100

    login validation

--- Client/src/components/ProtectedPage.js
+++ Client/src/components/ProtectedPage.js
@@ -1,39 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { message } from "antd";
 import { GetCurrentUser } from "../apicalls/users";
+import { useNavigate } from "react-router-dom";
 
-const ProtectedPage = ({children}) => {
-    const [user, setUser] = useState(null);
+const ProtectedPage = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
-    const validateToken = async () => {
-        try {
-            const response = await GetCurrentUser();
-            if (response.success) {
-                setUser(response.data);
-            } else {
-                message.error(response.message);
-            }
-        } catch (error) {
-            message.error(error.message);
-        }
-    };
-    useEffect(() => {
-        validateToken();
-    }, []);
+  const validateToken = async () => {
+    try {
+      const response = await GetCurrentUser();
+      if (response.success) {
+        setUser(response.data);
+      } else {
+        navigate("/login");
+      }
+    } catch (error) {
+      navigate("/login");
+    }
+  };
 
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      validateToken();
+    } else {
+      navigate("/login");
+    }
+  }, []);
 
-    return (
-        <div>
-            {user && 
-              (
-                <div className="p-5">
-                    {user.name}
-                    {children}
-                </div>
-              )
-            }
+  return (
+    <div>
+      {user && (
+        <div className="p-5">
+          {user.name}
+          {children}
         </div>
-    )
+      )}
+    </div>
+  );
 };
 
 export default ProtectedPage;
