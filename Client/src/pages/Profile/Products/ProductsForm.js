3ae1331d55f1e3f67a63da5b83381e85fa39803c
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-07-18 21:07:27 +0100

    products page

--- Client/src/pages/Profile/Products/ProductsForm.js
+++ Client/src/pages/Profile/Products/ProductsForm.js
@@ -1,20 +1,31 @@
-import Modal from 'antd/es/modal/Modal'
-import React from 'react'
+import { Form, Tabs } from "antd";
+import Input from "antd/es/input/Input";
+import Modal from "antd/es/modal/Modal";
+import React from "react";
 
-const ProductsForm = ({
-    showProductsForm,
-    setShowProductsForm
-}) => {
-
+const ProductsForm = ({ showProductsForm, setShowProductsForm }) => {
   return (
-    <Modal 
-    title = ''
-    open = {showProductsForm}
-    onCancel={()=>setShowProductsForm(false)}
-    centered
-    >      
+    <Modal
+      title=""
+      open={showProductsForm}
+      onCancel={() => setShowProductsForm(false)}
+      centered
+      width={1000}
+    >
+      <Tabs defaultActiveKey="1">
+        <Tabs.TabPane tab="General" key="1">
+          <Form layout="vertical">
+             <Form.Item label="Name" name='name'>
+                 <Input type="text" />
+             </Form.Item>
+          </Form>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Images" key="2">
+          <h1>Images</h1>
+        </Tabs.TabPane>
+      </Tabs>
     </Modal>
-  )
-}
+  );
+};
 
-export default ProductsForm
+export default ProductsForm;
