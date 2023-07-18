711bce0d34a3b0c876796026ca4395ce7dcd4d87
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-07-18 22:04:10 +0100

    products page

--- Client/src/pages/Profile/Products/ProductsForm.js
+++ Client/src/pages/Profile/Products/ProductsForm.js
@@ -1,7 +1,27 @@
-import { Form, Tabs } from "antd";
+import { Col, Form, Row, Tabs } from "antd";
 import Input from "antd/es/input/Input";
+import TextArea from "antd/es/input/TextArea";
 import Modal from "antd/es/modal/Modal";
 import React from "react";
+
+const additionalThings = [
+  {
+    label: "Bill Available",
+    name: "billAvailable",
+  },
+  {
+    label: "Warranty Available",
+    name: "warrantyAvailable",
+  },
+  {
+    label: "Accessories Available",
+    name: "accessoriesAvailable",
+  },
+  {
+    label: "Box Available",
+    name: "boxAvailable",
+  },
+];
 
 const ProductsForm = ({ showProductsForm, setShowProductsForm }) => {
   return (
@@ -15,9 +35,43 @@ const ProductsForm = ({ showProductsForm, setShowProductsForm }) => {
       <Tabs defaultActiveKey="1">
         <Tabs.TabPane tab="General" key="1">
           <Form layout="vertical">
-             <Form.Item label="Name" name='name'>
-                 <Input type="text" />
-             </Form.Item>
+            <Form.Item label="Name" name="name">
+              <Input type="text" />
+            </Form.Item>
+            <Form.Item label="Description" name="description">
+              <TextArea type="text" />
+            </Form.Item>
+            <Row gutter={[16, 16]}>
+              <Col span={8}>
+                <Form.Item label="Price" name="price">
+                  <Input type="number" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item label="Category" name="category">
+                  <select>
+                    <option value="electronics">Electronics</option>
+                    <option value="fashion">Fashion</option>
+                    <option value="home">Home</option>
+                    <option value="sports">Sports</option>
+                  </select>
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item label="Age" name="age">
+                  <Input type="number" />
+                </Form.Item>
+              </Col>
+            </Row>
+            <div className="flex gap-10">
+              {additionalThings.map((item) => {
+                return (
+                    <Form.Item label={item.label} name={item.name}>
+                       <Input type="checkbox" />
+                    </Form.Item>
+                )
+              })}
+            </div>
           </Form>
         </Tabs.TabPane>
         <Tabs.TabPane tab="Images" key="2">
