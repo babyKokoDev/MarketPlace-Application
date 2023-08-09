c919fb488ac65c78ea637dd1a5788da019bb362d
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-08-09 12:03:13 +0100

    home page

--- Client/src/pages/Home/index.js
+++ Client/src/pages/Home/index.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { SetLoader } from "../../redux/loaderSlice";
 import { getProducts } from "../../apicalls/products";
-import { message } from "antd";
+import { Divider, message } from "antd";
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -11,6 +12,7 @@ const Home = () => {
   });
   const { user } = useSelector((state) => state.users);
   const dispatch = useDispatch();
+  const navigate = useNavigate()
 
   const getData = async () => {
     try {
@@ -32,14 +34,27 @@ const Home = () => {
 
   return (
     <div>
-       {
+      <div className="grid grid-cols-5 gap-5">
+      {
         products?.map((product, index)=>{
-          return <div>
-             <h1>{product.name}</h1>
+          return <div className="border border-gray-300 rounded border-solid flex flex-col gap-2 pb-2 cursor-pointer" onClick={()=>{
+            navigate(`/products/${product._id}`)
+          }}>
+             <img src={product.images[0]} className="w-full h-40 object-cover" alt="" />
+
+             
+              <div className="px-2 flex-col gap-1">
+                <h1 className="text-lg font-semibold">{product.name}</h1>
+                <p className="text-sm">{product.description}</p>
+                <Divider/>
+                <span className="text-xl font-semibold text-green-700">
+                  ${product.price}
+                </span>
+              </div>
           </div>
         })
        }
-        
+      </div>  
     </div>
   );
 };
