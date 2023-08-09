17e94d7129fd3fa87909d0aedd9c88d47b533a05
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-08-09 09:26:56 +0100

    home page

--- Client/src/pages/Home/index.js
+++ Client/src/pages/Home/index.js
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { SetLoader } from "../../redux/loaderSlice";
+import { getProducts } from "../../apicalls/products";
+import { message } from "antd";
 
 const Home = () => {
-  const { user } = useSelector(state => state.users)
-  return (
-   <div>
-      <h1>HOME</h1>
-      {user && <h1>{user.name}</h1>}
-   </div>
-  )
-}
+  const [products, setProducts] = useState([]);
+  const [filters, setFilers] = useState({
+    status: "approved",
+  });
+  const { user } = useSelector((state) => state.users);
+  const dispatch = useDispatch();
 
-export default Home
+  const getData = async () => {
+    try {
+      dispatch(SetLoader(true));
+      const response = await getProducts(filters);
+      dispatch(SetLoader(false));
+      if (response.success) {
+        setProducts(response.products);
+      }
+    } catch (error) {
+      dispatch(SetLoader(false));
+      message.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+       {
+        products?.map((product, index)=>{
+          return <div>
+             <h1>{product.name}</h1>
+          </div>
+        })
+       }
+        
+    </div>
+  );
+};
+
+export default Home;
